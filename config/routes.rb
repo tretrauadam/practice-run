c2f3f5a750620c7062361758b59055c603b0b321
adamzermane <adamzermane92@yahoo.com>
2022-01-08 14:21:38 +0100

    rails-app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+   # root local host page
+  root to: 'pages#index'
+
+  get '/about', to: 'pages#about'
+  get '/contact', to: 'pages#contact'
 end
